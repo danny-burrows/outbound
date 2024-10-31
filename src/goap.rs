b65f4e0e60622318c842d7017fadc9dd77a48ffe
Danny Burrows <dannyburrows@protonmail.com>
2024-10-31 16:30:01 +0000

    Further sketching ideas for GOAP tree construction via Actions

--- src/goap.rs
+++ src/goap.rs
@@ -1,4 +1,11 @@
-// The objective of GOAP is to find a way from the current State -> goal State through Actions.
+/// The objective of GOAP is for an `Agent` to find a way from the current `State` -> goal `State` through `Action`s.
+use std::collections::HashMap;
+
+#[derive(Clone)]
+struct Agent {
+    position: (f64, f64),
+    inventory: HashMap<String, i64>,
+}
 
 // State MUST be all-encompassing
 // e.g. For an agent to pick something up; the information about the item, where it is, and the agent's inventory must all be included in the state.
@@ -6,36 +13,59 @@
 // - States can then be augmented with agent perception.
 #[derive(Clone)]
 struct State {
-    trees: u32,
-    food: i32,
+    agent: Agent,
+    items: Vec<String>,
 }
 
 // Actions describe changes to the input State and can be generated on the fly. For example, a MoveAction moves the Agent toward a certain item.
 trait Action {
-    fn act(&self, current_state: State) -> State {
-        todo!()
+    fn act(&self, current_state: State) -> State;
+
+    fn cost(&self) -> f64;
+
+    fn prerequisite(&self) -> Option<()> {
+        None
     }
 }
 
 struct MoveAction {
-    delta_x: i32,
-    delta_y: i32,
+    delta_x: f64,
+    delta_y: f64,
 }
 
 impl Action for MoveAction {
     fn act(&self, current_state: State) -> State {
         let mut new_state = current_state.clone();
+        new_state.agent.position = (
+            new_state.agent.position.0 + self.delta_x,
+            new_state.agent.position.1 + self.delta_y,
+        );
+        new_state
+    }
 
-        // new_state.pos.x += self.delta_x;
-        // new_state.pos.y += self.delta_y;
-        // new_state
+    fn cost(&self) -> f64 {
+        (self.delta_x.powf(2.0) + self.delta_y.powf(2.0)).sqrt()
+    }
+}
 
-        todo!()
+struct PickUpAction {
+    item_id: String,
+}
+
+impl Action for PickUpAction {
+    fn act(&self, current_state: State) -> State {
+        let mut new_state = current_state.clone();
+        new_state.agent.inventory.insert(self.item_id.clone(), 1);
+        new_state
+    }
+
+    fn cost(&self) -> f64 {
+        1.0
     }
 }
 
 trait Planner {
-    const PATIENTS: u32 = 100;
+    const PATIENCE: u32 = 100;
     type Plan: Iterator<Item = Box<dyn Action>>;
 
     fn plan(
@@ -49,4 +79,8 @@ trait Planner {
 
         todo!()
     }
+}
+
+struct Node<'a> {
+    next: &'a Node<'a>,
 }
